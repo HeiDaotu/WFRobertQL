7130822fd1d72f236a09dfb1ce73ceb9984383e6
heidaotu <qq847718367>
2023-05-19 10:21:11 +0800

    顺丰速运调整为维护状态

--- sfsy/sfsy.py
+++ sfsy/sfsy.py
@@ -9,19 +9,5 @@ new Env('顺丰速运 v1.03');
 Description: 顺丰速运做任务
 Update: 2023/5/5 更新cron
 """
-import os
-import time
-import uuid
-import requests
 
-
-def get_cookies():
-    if os.environ.get("DDNSTO_COOKIE"):
-        print("🍪已获取并使用Env环境 Cookie")
-        return os.environ.get("DDNSTO_COOKIE")
-    return None
-
-
-if __name__ == "__main__":
-    # 使用format方法格式化字符串
-    cookie = get_cookies()
+print("🍪顺丰速运脚本维护中")
