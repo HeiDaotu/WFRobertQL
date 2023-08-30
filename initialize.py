3d13c8ecb049cd40a4ff025a58665a0ab73c5926
HeiDaoTu <409268192@qq.com>
2023-08-30 19:37:28 +0800

    😎初始化文件修改

--- initialize.py
+++ initialize.py
@@ -74,10 +74,10 @@ def init():
 
     :return:
     """
-    logging.info("开启10秒到5分钟之间的随机延迟时间")
-    delay = random.uniform(10, 500)
-    logging.info(f"开启延迟，{delay}秒后执行代码")
-    time.sleep(delay)
-
     # 初始化日志
     init_logger()
+    # 随机延迟
+    logging.info("开启10秒到5分钟之间的随机延迟时间，如果不需要延迟  请将initialize.py代码中的最后一行代码注释掉")
+    delay = random.uniform(10, 500)
+    logging.info(f"开启延迟，{delay}秒后执行代码")
+    time.sleep(delay)  # 注释该行代码，即可不会有延迟
