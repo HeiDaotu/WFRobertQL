4f7ae93f1a02ac8d5db0e2ee072c44ec5dfbc0a7
WFRobert <409268192@qq.com>
2023-09-27 12:48:57 +0800

    😊初始化文件修改日志显示

--- initialize.py
+++ initialize.py
@@ -78,6 +78,6 @@ def init():
     init_logger()
     # 随机延迟
     logging.info("开启10秒到5分钟之间的随机延迟时间，如果不需要延迟  请将initialize.py代码中的最后一行代码注释掉")
-    delay = random.uniform(10, 300)
+    delay = int(random.uniform(10, 300))
     logging.info(f"开启延迟，{delay}秒后执行代码")
     time.sleep(delay)  # 注释该行代码，即可不会有延迟
